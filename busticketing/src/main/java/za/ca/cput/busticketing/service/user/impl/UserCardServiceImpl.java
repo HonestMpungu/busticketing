9323293941f7ac27601acec946b49b643358d2d4
Manyati <joelmunya1000@gmail.com>
2021-10-23 06:41:06 +0200

    checking git connection

--- busticketing/src/main/java/za/ca/cput/busticketing/service/user/impl/UserCardServiceImpl.java
+++ busticketing/src/main/java/za/ca/cput/busticketing/service/user/impl/UserCardServiceImpl.java
@@ -18,7 +18,7 @@ import java.util.Optional;
  */
 @Service
 public class UserCardServiceImpl implements UserCardService {
-    private UserCardRepository userCardRepository;
+    private UserCardRepository  userCardRepository;
 
     @Autowired
     public UserCardServiceImpl(UserCardRepository userCardRepository) {
