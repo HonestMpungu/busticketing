694b74ca8b34fdbc99b7b7ab4207029ceffe96c2
HonestMpungu <Mpunguhonest@gmail.com>
2021-10-23 07:40:10 +0200

    Committ Frontend#3

---
+++ busticketing/src/test/java/za/ca/cput/busticketing/controller/card/CardTypeControllerTest.java
@@ -0,0 +1,32 @@
+package za.ca.cput.busticketing.controller.card;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTypeControllerTest {
+
+    @Test
+    void viewCardTypesPage() {
+    }
+
+    @Test
+    void addCardType() {
+    }
+
+    @Test
+    void displayAddCardTypeForm() {
+    }
+
+    @Test
+    void showUpdateForm() {
+    }
+
+    @Test
+    void updateCardType() {
+    }
+
+    @Test
+    void deleteCardType() {
+    }
+}
